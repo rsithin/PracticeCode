9e6c0290d7244339aeb030f3c4dfe3e547238a2a
rsithi00 <70171298+rsithi00@users.noreply.github.com>
2023-08-24 23:41:47 -0500

    small changes

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -125,6 +125,7 @@ public class Algorithms {
      * This method merges the array in ascending order
      */
     public static void merge(int[] a, int p, int q, int r) {
+        
         int n1 = q - p + 1;
         int n2 = r - q;
 
---
+++ src/Practice.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Practice {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6,7,8,9};
+
+        System.out.printf("Here we are: %s\n", Arrays.toString(a));
+    }
+    
+}
